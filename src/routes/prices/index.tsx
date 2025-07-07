7eeb210913c8934b73806ad81237ea7573dd7f6d
Duong Pham <duongpham@mac.home>
2025-07-07 20:10:13 +0100

    prices;

--- src/routes/prices/index.tsx
+++ src/routes/prices/index.tsx
@@ -1,6 +1,6 @@
 import styles from './Prices.module.scss';
 import React from 'react';
-import {Props} from 'pages/prices';
+import {Props} from 'prices';
 
 import Introduction from './introduction';
 import Items from './items';
--- src/routes/prices/items/index.tsx
+++ src/routes/prices/items/index.tsx
@@ -1,6 +1,6 @@
 import styles from './Items.module.scss';
 import React from 'react';
-import {Props} from 'pages/prices';
+import {Props} from 'prices';
 import useOpen from '@hooks/useOpen';
 
 import { MdKeyboardArrowRight } from 'react-icons/md';
