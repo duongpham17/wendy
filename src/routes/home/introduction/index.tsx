af9f3fd3e755b0e1be502afddc2d0edef9dccd45
Duong Pham <duongpham17@icloud.com>
2026-04-04 15:11:26 +0100

    bg

--- src/routes/home/introduction/index.tsx
+++ src/routes/home/introduction/index.tsx
@@ -6,15 +6,13 @@ import Link from '@components/link/Style2';
 const Introduction = () => {
   return (
     <div className={styles.container}>
-        <section>
 
-            <h1>Welcome</h1>
+      <section className={styles.content}>
+        <h1>Welcome</h1>
+        <Link href="/services" value="discover our services" />
+        <small>Founded in 2006</small>
+      </section>
 
-            <Link href="/services" value="discover our services" />
-
-            <small>Founded in 2006</small>
-
-        </section>
     </div>
   )
 }
